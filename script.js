51e9748d847751b6daeabc0820d6df2268537619
atharv5167 <aus588185@gmail.com>
2026-03-09 21:19:13 +0530

    Add Bakingo website files

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlert(item) {
+    console.log(`Clicked: ${item}`);
+}
+
+function selectCake(cakeName) {
+    console.log(`Selected: ${cakeName}`);
+}
+
+function orderNow() {
+    alert('🎉 Order now redirects to checkout page!');
+}
