e101974b1832d837003d322d7a239f8cfe097ba0
atharv5167 <aus588185@gmail.com>
2026-03-10 20:59:03 +0530

    img in home page

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) currentIndex = 0;
+    else if (index < 0) currentIndex = totalSlides - 1;
+    else currentIndex = index;
+
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Auto slide
+setInterval(nextSlide, 3000);
+
 function showAlert(item) {
     console.log(`Clicked: ${item}`);
 }
